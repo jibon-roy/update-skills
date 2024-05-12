f94436c32b330c80218e3cebf4f1dce87e6bfa5a
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-13 01:59:17 +0600

    added a test schema with mongoose

--- src/lib/models/postModel.js
+++ src/lib/models/postModel.js
@@ -1,3 +1,15 @@
 import { Schema, model, models } from "mongoose";
 
-const postSchema = new Schema({});
+const postSchema = new Schema(
+  {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const PostModel = models.user || model("post", postSchema);
+
+export default PostModel;
