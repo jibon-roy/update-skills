a7e3576190c8efa5bc03006c36587c7240333ddf
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-14 02:11:42 +0600

    next config mjs update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn-icons-png.freepik.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
