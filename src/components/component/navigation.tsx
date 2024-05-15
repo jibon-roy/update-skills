3b7a50e8aba40790738527e1a2f220d1dc026188
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-15 20:49:45 +0600

    about route update

--- src/components/component/navigation.tsx
+++ src/components/component/navigation.tsx
@@ -24,7 +24,7 @@ export function Navigation() {
             <Link className="font-medium flex items-center text-sm transition-colors hover:underline" href="/dashboard">
               Dashboard
             </Link>
-            <Link className="font-medium flex items-center text-sm transition-colors hover:underline" href="#">
+            <Link className="font-medium flex items-center text-sm transition-colors hover:underline" href="/about">
               About
             </Link>
             <Link className="font-medium flex items-center text-sm transition-colors hover:underline" href="#">
