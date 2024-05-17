7cc5864faf3bcb7f6ea1fe502fc4004bd91e384c
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-17 23:16:34 +0600

    tailconfigjs update #2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,11 @@ const config = {
         "2xl": "1400px",
       },
     },
+    duration: {
+            fast: "1s",
+            normal: "3s",
+            slow: "5s",
+        },
     extend: {
       colors: {
         border: "hsl(var(--border))",
