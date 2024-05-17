ec7c3393c961a4d4733d6001abfde416706866c8
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-17 23:16:05 +0600

    tailconfigjs update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,7 +74,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"),  require("@designbycode/tailwindcss-text-glitch"),],
 } satisfies Config
 
 export default config
