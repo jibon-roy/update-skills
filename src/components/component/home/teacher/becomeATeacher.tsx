9979ece592f6821fa0c474e7ea16de0f9c66a274
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-22 23:51:30 +0600

    br added to teacher section

--- src/components/component/home/teacher/becomeATeacher.tsx
+++ src/components/component/home/teacher/becomeATeacher.tsx
@@ -17,7 +17,7 @@ function BecomeATeacher({ }: Props) {
             heading="Start Your Teaching Career With Us"
         >
             Teachers from around the world teach millions of learners on Update-Skills. We provide the tools and skills to teach what you love.
-            <br />
+            <br /> <br />
 
             We are founding that person who actively love to teach and share knowledge with others.
             <br />
