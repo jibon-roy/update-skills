dd485498c7f0f8d8f63d5bb29cb66b312c04089d
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-23 01:02:03 +0600

    button add for google login

--- src/app/(MainLayout)/login/page.tsx
+++ src/app/(MainLayout)/login/page.tsx
@@ -4,6 +4,7 @@ import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Player } from '@lottiefiles/react-lottie-player';
+import { FcGoogle } from "react-icons/fc";
 
 function LoginPage() {
     return (
@@ -17,33 +18,37 @@ function LoginPage() {
                         style={{ height: '300px', width: '300px' }}
                         autoplay
                         loop
-
                     >
                     </Player>
 
                 </div>
             </div>
-            <form className="flex items-center flex-col bg-gradient-to-top justify-center p-8">
+            <div className="flex items-center flex-col bg-gradient-to-top justify-center p-8">
                 <Card className="w-full max-w-md">
                     <CardHeader className="space-y-1">
                         <CardTitle className="text-2xl text-white">Login to your account</CardTitle>
                         <CardDescription className="text-white">Enter your email and password to access your account.</CardDescription>
                     </CardHeader>
-                    <CardContent className="space-y-4">
-                        <div className="space-y-2">
-                            <Label htmlFor="email">Email</Label>
-                            <Input id="email" placeholder="m@example.com" type="email" />
-                        </div>
-                        <div className="space-y-2">
-                            <Label htmlFor="password">Password</Label>
-                            <Input id="password" type="password" />
-                        </div>
-                    </CardContent>
-                    <CardFooter>
-                        <Button className="w-full">Sign In</Button>
-                    </CardFooter>
+                    <form action="">
+                        <CardContent className="space-y-4 p-6">
+                            <div className="space-y-2  text-white">
+                                <Label htmlFor="email">Email</Label>
+                                <Input id="email" autoComplete="email" placeholder="m@example.com" type="email" />
+                            </div>
+                            <div className="space-y-2  text-white">
+                                <Label htmlFor="password">Password</Label>
+                                <Input id="password" autoComplete="current-password" type="password" />
+                            </div>
+                        </CardContent>
+                        <CardFooter>
+                            <Button className="w-full">Sign In</Button>
+                        </CardFooter>
+                    </form>
+                    <div>
+                        <Button variant={"secondary"}><FcGoogle className="text-xl mr-2" /> Continue With Google</Button>
+                    </div>
                 </Card>
-            </form>
+            </div>
         </div>
     )
 }
