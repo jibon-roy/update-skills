aaafefba6b63917602617e0ebcd51815a2766a5e
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-24 18:00:29 +0600

    updating footer title

--- src/components/component/footer.tsx
+++ src/components/component/footer.tsx
@@ -11,7 +11,7 @@ export function Footer() {
             <span className="text-lg font-semibold text-gray-300">Update Skills</span>
           </div>
           <p className="text-sm leading-relaxed">
-            Acme Inc is a leading provider of innovative solutions for businesses of all sizes.
+            Updat is a leading provider of innovative solutions for businesses of all sizes.
           </p>
         </div>
         <div className="grid gap-2">
