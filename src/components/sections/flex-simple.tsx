9e2474cf1503c661f19fa3dbc4f506ae795f9bcc
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-26 00:25:51 +0600

    flexsimple editing

--- src/components/sections/flex-simple.tsx
+++ src/components/sections/flex-simple.tsx
@@ -1,30 +1,49 @@
+"use client"
 import CSS from 'csstype';
+import { useEffect, useState } from 'react';
 type Props = {
     children?: any;
-    heading?: string;
-    head?: string;
-    description?: string;
     bg?: string;
     className?: string;
-    imgContent?: string;
     justifyContent: 'center' | 'space-between' | 'space-evenly' | 'space-around';
     flexDirectionMd: "column" | "row" | "column-reverse" | "row-reverse";
     flexDirection: "column" | "row" | "column-reverse" | "row-reverse";
     textColor?: string;
 }
 
-function FlexSimple({ flexDirection, flexDirectionMd, justifyContent, bg, children, className, description, head, heading, imgContent
+function FlexSimple({ flexDirection, flexDirectionMd, textColor, justifyContent, bg, children, className,
 }: Props) {
+
+    const [direction, setDirection] = useState(flexDirection);
+
+    useEffect(() => {
+        const updateBackground = () => {
+            if (window.matchMedia('(max-width: 1024px)').matches) {
+                setDirection(flexDirection);
+            } else {
+                setDirection(flexDirectionMd);
+            }
+        };
+        updateBackground();
+        window.addEventListener('resize', updateBackground);
+        return () => {
+            window.removeEventListener('resize', updateBackground);
+        };
+    }, [flexDirection, flexDirectionMd]);
+
+
     const styles: CSS.Properties = {
+        flexDirection: direction,
+        justifyContent: justifyContent,
         display: "flex",
-        flexDirection: flexDirection,
+        WebkitJustifyContent: justifyContent,
+        color: textColor
     }
     return (
         <div style={{ backgroundColor: bg }}>
             <div className={className} style={styles}>
-
+                {children}
             </div>
-
         </div>
     )
 };
