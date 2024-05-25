8f310b9e83268fa0a79f50913b1e374ea5a168c7
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-25 23:43:01 +0600

    updating svg

--- src/components/component/statistics.tsx
+++ src/components/component/statistics.tsx
@@ -85,10 +85,10 @@ function DollarSignIcon(props: any) {
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
-      viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      viewBox="0 0 24 24"
       strokeWidth="2"
+      stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
     >
