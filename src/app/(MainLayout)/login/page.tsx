e03d9607f9fa3acbb5340fa607ecb4b264d3b2d0
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-26 13:36:06 +0600

    login page navigation updating

--- src/app/(MainLayout)/login/page.tsx
+++ src/app/(MainLayout)/login/page.tsx
@@ -6,8 +6,25 @@ import { Button } from "@/components/ui/button"
 import { Player } from '@lottiefiles/react-lottie-player';
 import { FcGoogle } from "react-icons/fc";
 import { Separator } from "@/components/ui/separator"
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+// import { useRouter } from 'next/router';
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 function LoginPage() {
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            console.log(window.history);
+        }
+    }, []);
+
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.back();
+    };
+
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 h-[100dvh]">
             <div className="hidden lg:flex flex-col justify-center items-center">
@@ -21,16 +38,18 @@ function LoginPage() {
                         loop
                     >
                     </Player>
-
                 </div>
             </div>
-            <div className="flex items-center flex-col bg-gradient-to-top justify-center p-8">
+            <div className="flex relative items-center flex-col bg-gradient-to-top justify-center p-8">
+                <Button onClick={handleClick} className="flex absolute top-5 right-5 gap-2 group" >
+                    <FaArrowAltCircleLeft className="group-hover:-translate-x-1" /> Back
+                </Button>
                 <Card className="w-full max-w-md">
                     <CardHeader className="space-y-1">
                         <CardTitle className="text-4xl text-white">Login to your account</CardTitle>
                         <CardDescription className="text-white">Enter your email and password to access your account.</CardDescription>
                     </CardHeader>
-                    <form action="">
+                    <form>
                         <CardContent className="space-y-4 p-6">
                             <div className="space-y-2  text-white">
                                 <Label htmlFor="email">Email</Label>
@@ -45,8 +64,8 @@ function LoginPage() {
                             <Button className="w-full">Sign In</Button>
                         </CardFooter>
                     </form>
-                    <div className="text-2xl flex justify-center font-bold mb-4 text-white">
-                        <Separator aria-placeholder="or"></Separator>
+                    <div className="text-2xl flex justify-center items-center gap-4 font-bold mb-4 text-white">
+                        <Separator className="w-20" aria-placeholder="or"></Separator>or<Separator className="w-20" aria-placeholder="or"></Separator>
                     </div>
                     <div className="flex justify-center">
                         <Button variant={"secondary"}><FcGoogle className="text-xl mr-2" /> Continue With Google</Button>
