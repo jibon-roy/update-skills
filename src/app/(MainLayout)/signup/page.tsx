0fece7982688c259659f788d197e25a919d0f099
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-28 02:22:14 +0600

    password status seted

--- src/app/(MainLayout)/signup/page.tsx
+++ src/app/(MainLayout)/signup/page.tsx
@@ -17,6 +17,7 @@ type Props = {}
 function SignUp({ }: Props) {
     const [passType, setPassType] = useState('password')
     const [passError, setPassError] = useState('')
+    const [passwordStatus, setPassStatus] = useState('')
 
     const handleRegister = (event: any) => {
         event.preventDefault();
@@ -71,18 +72,10 @@ function SignUp({ }: Props) {
         }
     }
 
-    const checkPassword = (e: any) => {
-        const checkPass = passwordStrength(e.target.value)
-        if (!checkPass.contains.includes('lowercase')
-            || !checkPass.contains.includes('number')
-            || !checkPass.contains.includes('symbol')
-            || !checkPass.contains.includes('uppercase')
-        ) {
-            setPassError('Password must contain number, symbol, uppercase and lowercase.')
-            return;
-        } else {
-            setPassError('')
-        }
+    const passStatus = (e: any) => {
+        const testPass = passwordStrength(e.target.value)
+        console.log(testPass)
+        setPassStatus(testPass.value)
     }
 
 
@@ -108,23 +101,23 @@ function SignUp({ }: Props) {
                         <Input required name="dateOfBirth" id="dateOfBirth" max={formatDate(new Date())} className="text-black transition-all focus:outline-main-primary-yellow focus-within:outline-main-primary-yellow focus-visible:outline-main-primary-yellow" autoComplete="date" type="date" />
                     </div>
                     <div className="text-white">
-                        <Label htmlFor="" className="text-white">Gender</Label>
+                        <div className="text-white text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Gender</div>
                         <div className="flex justify-between">
-                            <Label htmlFor="gender" className="flex gap-2 items-center">
+                            <Label htmlFor="male" className="flex gap-2 items-center">
                                 <Input value="Male" required defaultChecked name="gender" id="male" className="text-black cursor-pointer w-5 transition-all focus:outline-main-primary-yellow focus-within:outline-main-primary-yellow focus-visible:outline-main-primary-yellow" type="radio" />
                                 Male</Label>
-                            <Label htmlFor="gender" className="flex gap-2 items-center">
+                            <Label htmlFor="female" className="flex gap-2 items-center">
                                 <Input value="Female" name="gender" id="female" className="text-black w-5 transition-all cursor-pointer focus:outline-main-primary-yellow focus-within:outline-main-primary-yellow focus-visible:outline-main-primary-yellow" type="radio" />
                                 Female</Label>
-                            <Label htmlFor="gender" className="flex gap-2 items-center">
+                            <Label htmlFor="others" className="flex gap-2 items-center">
                                 <Input value="Others" name="gender" id="others" className="text-black w-5 transition-all cursor-pointer focus:outline-main-primary-yellow focus-within:outline-main-primary-yellow focus-visible:outline-main-primary-yellow" type="radio" />
                                 Others</Label>
                         </div>
                     </div>
                     <div className="space-y-2 relative text-white">
                         <FaEye onClick={showPass} className="absolute right-3  top-11 cursor-pointer w-8 text-black"></FaEye>
-                        <Label htmlFor="password">Password</Label>
-                        <Input name="password" id="password" placeholder="Password" className="text-black transition-all focus:outline-main-primary-yellow focus-within:outline-main-primary-yellow focus-visible:outline-main-primary-yellow" autoComplete="current-password" type={passType} />
+                        <Label htmlFor="password" className="">Password: <span className={passwordStatus === 'Strong' ? 'text-green-400' : passwordStatus === 'Medium' ? 'text-main-primary-yellow' : passwordStatus === 'Weak' ? 'text-orange-400' : 'text-red-500'}>{passwordStatus}</span></Label>
+                        <Input onChange={passStatus} name="password" id="password" placeholder="Password" className="text-black transition-all focus:outline-main-primary-yellow focus-within:outline-main-primary-yellow focus-visible:outline-main-primary-yellow" autoComplete="current-password" type={passType} />
                         <div className={"hidden" + passError && "block w-full border-blue-500 text-red-500"}>{passError}</div>
                     </div>
                 </CardContent>
