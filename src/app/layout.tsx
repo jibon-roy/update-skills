0ef1f7e3c11e04ba0381b04ef79a65c33dcaf374
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-29 00:54:25 +0600

    session add to props in session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import StoreProvider from "./redux/storeProvider";
-
+import AuthProvider from "@/lib/utils/SessionProvider";
+import { getServerSession } from "next-auth";
 
 export const metadata: Metadata = {
     title: "Update Skills",
@@ -10,18 +11,21 @@ export const metadata: Metadata = {
 };
 
 
-
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
 
+    const session = await getServerSession()
+
     return (
         <StoreProvider>
             <html lang="en">
                 <body className="bg-primary-foreground">
-                    {children}
+                    <AuthProvider session={session}>
+                        {children}
+                    </AuthProvider>
                 </body>
             </html>
         </StoreProvider>
--- src/lib/utils/SessionProvider.tsx
+++ src/lib/utils/SessionProvider.tsx
@@ -1,12 +1,12 @@
 "use client"
-import React from 'react'
 import { SessionProvider } from 'next-auth/react'
 type Props = {
-    children: any
+    children: any,
+    session: any,
 }
 
-function AuthProvider({ children }: Props) {
-    return <SessionProvider>{children}</SessionProvider>
+function AuthProvider({ children, session }: Props) {
+    return <SessionProvider session={session}>{children}</SessionProvider>
 };
 
 export default AuthProvider;
