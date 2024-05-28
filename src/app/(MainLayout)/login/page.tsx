accca4961e80409f43b98d35ce01ff811c68db93
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-29 00:01:35 +0600

    removed un wanted coomponents and tags from login

--- src/app/(MainLayout)/login/page.tsx
+++ src/app/(MainLayout)/login/page.tsx
@@ -26,38 +26,26 @@ function LoginPage() {
             description="Enter your email and password to access your account."
             lottieFilesLink="https://lottie.host/6dfbcf18-59a0-4b14-b66c-36d54c7256fd/WNjGwIBInf.json"
         >
-
-            <div className="flex relative items-center flex-col bg-gradient-to-top justify-center px-2 md:px-8 py-12">
-                <Button onClick={handleClick} className="flex md:absolute top-5 right-5 gap-2 group" >
-                    <FaArrowAltCircleLeft className="group-hover:-translate-x-1" /> Back to Home
-                </Button>
-                <Card className="w-full max-w-md">
-                    <CardHeader className="space-y-1">
-                        <CardTitle className="text-4xl text-white"></CardTitle>
-                        <CardDescription className="text-white"></CardDescription>
-                    </CardHeader>
-                    <form>
-                        <CardContent className="space-y-4 p-6">
-                            <div className="space-y-2  text-white">
-                                <Label htmlFor="email">Email</Label>
-                                <Input id="email" className="text-black transition-all focus:outline-main-primary-yellow focus-within:outline-main-primary-yellow focus-visible:outline-main-primary-yellow" autoComplete="email" placeholder="example@mail.com" type="email" />
-                            </div>
-                            <div className="space-y-2  text-white">
-                                <Label htmlFor="password">Password</Label>
-                                <Input id="password" placeholder="Password" className="text-black transition-all focus:outline-main-primary-yellow focus-within:outline-main-primary-yellow focus-visible:outline-main-primary-yellow" autoComplete="current-password" type="password" />
-                            </div>
-                        </CardContent>
-                        <CardFooter>
-                            <Button className="w-full">Sign In</Button>
-                        </CardFooter>
-                    </form>
-                    <div className="text-2xl flex justify-center items-center gap-4 font-bold mb-4 text-white">
-                        <Separator className="w-20" aria-placeholder="or"></Separator>or<Separator className="w-20" aria-placeholder="or"></Separator>
+            <form>
+                <CardContent className="space-y-4 p-6">
+                    <div className="space-y-2  text-white">
+                        <Label htmlFor="email">Email</Label>
+                        <Input id="email" className="text-black transition-all focus:outline-main-primary-yellow focus-within:outline-main-primary-yellow focus-visible:outline-main-primary-yellow" autoComplete="email" placeholder="example@mail.com" type="email" />
                     </div>
-                    <div className="flex justify-center">
-                        <Button variant={"secondary"}><FcGoogle className="text-xl mr-2" /> Continue With Google</Button>
+                    <div className="space-y-2  text-white">
+                        <Label htmlFor="password">Password</Label>
+                        <Input id="password" placeholder="Password" className="text-black transition-all focus:outline-main-primary-yellow focus-within:outline-main-primary-yellow focus-visible:outline-main-primary-yellow" autoComplete="current-password" type="password" />
                     </div>
-                </Card>
+                </CardContent>
+                <CardFooter>
+                    <Button className="w-full">Sign In</Button>
+                </CardFooter>
+            </form>
+            <div className="text-2xl flex justify-center items-center gap-4 font-bold mb-4 text-white">
+                <Separator className="w-20" aria-placeholder="or"></Separator>or<Separator className="w-20" aria-placeholder="or"></Separator>
+            </div>
+            <div className="flex justify-center">
+                <Button variant={"secondary"}><FcGoogle className="text-xl mr-2" /> Continue With Google</Button>
             </div>
         </AuthPage>
     )
