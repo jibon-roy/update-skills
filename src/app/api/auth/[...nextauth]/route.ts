6a915e21c487f77b1ba6760c00c83f22e4d7658c
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-28 23:50:11 +0600

    credential proveder add

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,37 @@
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth/next";
 import { Account, User as AuthUser } from "next-auth";
-import { CredentialsProvider } from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
 import User from "@/lib/models/userModel";
 import connectDB from "@/lib/db/connectDB";
+import bcrypt from 'bcrypt'
 
 export const authOptions: any ={
     providers: [
         CredentialsProvider({
-
+            id: 'credentials',
+            name: 'Credentials',
+            credentials: {
+                email: { label: 'Email', type: 'text' },
+                password: {label: 'Password', type: 'password'}
+            },
+            async authorize(credentials: any) {
+                await connectDB()
+                try {
+                    const user: any = User.findOne({ email: credentials.email })
+                    if (user) {
+                        const isPasswordCorrect = await bcrypt.compare(
+                            credentials.password,
+                            user.password
+                        )
+                        if (isPasswordCorrect) {
+                            return user;
+                        }
+                    }
+                } catch (error: any) {
+                    throw new Error(error);
+                }
+            }
         }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
