300f0622d67d488ecee1ed6edc36270c07a606fa
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-30 11:04:57 +0600

    about menu edited

--- src/components/component/navigation.tsx
+++ src/components/component/navigation.tsx
@@ -52,7 +52,10 @@ export function Navigation() {
               `before:content-[''] before:absolute before:-bottom-1 before:left-0 before:w-full before:h-1 before:bg-main-primary-violet hover:before:scale-x-100 before:scale-x-0 before:origin-left before:transition-transform before:duration-300 before:ease-in-out`}`}>
               Dashboard
             </Link>
-            <Link href="/about" className={`font-medium flex items-center text-sm transition-colors hover:underline ${isActive('/about') ? 'font-bold underline' : ''}`}>
+            <Link href="/about" className={`font-medium flex items-center text-sm transition-colors relative  ${isActive('/about') ?
+              ` before:content-[''] before:absolute before:-bottom-1 before:left-0 before:w-full before:h-1 before:bg-main-primary-violet before:duration-300 before:ease-in-out`
+              :
+              `before:content-[''] before:absolute before:-bottom-1 before:left-0 before:w-full before:h-1 before:bg-main-primary-violet hover:before:scale-x-100 before:scale-x-0 before:origin-left before:transition-transform before:duration-300 before:ease-in-out`}`}>
               About
             </Link>
             <Link href="/services" className={`font-medium flex items-center text-sm transition-colors hover:underline ${isActive('/services') ? 'font-bold underline' : ''}`}>
