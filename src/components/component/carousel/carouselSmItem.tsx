dbe78d2a12da304b31542a4407a306422cd69db7
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-06-02 01:02:50 +0600

    carousel updated

--- src/components/component/carousel/carouselSmItem.tsx
+++ src/components/component/carousel/carouselSmItem.tsx
@@ -12,8 +12,6 @@ function CarouselSmItem({ children }: Props) {
         <Carousel
             additionalTransfrom={0}
             arrows
-            autoPlay
-            autoPlaySpeed={1000}
             centerMode={false}
             className=""
             containerClass="container-with-dots"
@@ -34,15 +32,16 @@ function CarouselSmItem({ children }: Props) {
                         max: 3000,
                         min: 1024
                     },
-                    items: 3,
+                    items: 5,
                     partialVisibilityGutter: 40
                 },
+
                 mobile: {
                     breakpoint: {
                         max: 464,
                         min: 0
                     },
-                    items: 1,
+                    items: 2,
                     partialVisibilityGutter: 30
                 },
                 tablet: {
@@ -50,7 +49,7 @@ function CarouselSmItem({ children }: Props) {
                         max: 1024,
                         min: 464
                     },
-                    items: 2,
+                    items: 3,
                     partialVisibilityGutter: 30
                 }
             }}
