0014ffb3b7779cd4fa0f33281b626d2d894aec85
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-06-02 01:13:04 +0600

    explore categories div updating

--- src/app/(MainLayout)/allcourses/page.tsx
+++ src/app/(MainLayout)/allcourses/page.tsx
@@ -3,6 +3,7 @@ import CarouselSmItem from "@/components/component/carousel/carouselSmItem";
 import PageHeader from "@/components/component/pageHeader/pageHeader";
 import DynamicCard from "@/components/ui/customizeCard";
 import Image from "next/image";
+import { FaBrain } from "react-icons/fa";
 
 
 type Props = {}
@@ -15,14 +16,10 @@ function AllCourses({ }: Props) {
                     Explore Categories:
                 </div>
                 <CarouselSmItem>
-                    <DynamicCard>
-                        <Image
-                            src="https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-                            width={300}
-                            height={300}
-                            priority
-                            alt="React Carousel with Server Side Rendering Support Part 2"
-                        />
+                    <DynamicCard className="bg-white text-black rounded-lg transition cursor-pointer p-4 m-2 hover:bg-main-primary-yellow">
+                        <div>
+                            <FaBrain />
+                        </div>
                     </DynamicCard>
                     <DynamicCard>
                         <Image
