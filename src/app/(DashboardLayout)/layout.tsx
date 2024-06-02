b3dae20cbdff07c2369aecf9358a5c8172d58aa5
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-06-02 14:34:02 +0600

    dashboard layout update

--- src/app/(DashboardLayout)/layout.tsx
+++ src/app/(DashboardLayout)/layout.tsx
@@ -23,14 +23,14 @@ export default function RootDashboardLayout({
     return (
         <main>
             <PrivateRoute>
-                <div className="lg:flex relative overflow-x-hidden min-h-screen w-full">
-                    <div className={`inset-y-0 left-0 bg-slate-700 shadow-lg z-10 transform ${isMenuOpen ? 'w-56 sm:w-64 px-2' : 'w-0 p-0 sm:w-14'
+                <div className="lg:flex text-white overflow-x-hidden w-full">
+                    <div className={`left-0 bg-slate-700 shadow-lg z-10 transform ${isMenuOpen ? 'w-56 sm:w-64 px-2' : 'w-0 p-0 sm:w-14'
                         } absolute transition-all p-0 sm:px-2 sm:hover:w-64 bg-slate-700 duration-300 ease-in-out`} >
                         <DashboardNavigation handleOpenNav={handleOpenNav} />
                     </div>
                     <div className={`flex-1 overflow-hidden ${isMenuOpen ? 'sm:ml-64' : 'sm:ml-14'} transition-all duration-300 ease-in-out`}>
                         <DashboardHeaderNav handleOpenNav={handleOpenNav} />
-                        <div className="p-2 sm:p-4">
+                        <div className="p-2">
                             {children}
                         </div>
                     </div>
