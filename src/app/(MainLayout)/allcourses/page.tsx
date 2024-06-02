babb76adb63689553800ba217b339e8d57430a75
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-06-02 13:17:35 +0600

    all courses page update caroucel

--- src/app/(MainLayout)/allcourses/page.tsx
+++ src/app/(MainLayout)/allcourses/page.tsx
@@ -1,9 +1,8 @@
 "use client"
 import CarouselSmItem from "@/components/component/carousel/carouselSmItem";
+import SmCard from "@/components/component/carousel/smCard";
 import PageHeader from "@/components/component/pageHeader/pageHeader";
-import DynamicCard from "@/components/ui/customizeCard";
-import Image from "next/image";
-import { FaGlobe } from "react-icons/fa";
+
 
 
 type Props = {}
@@ -16,106 +15,11 @@ function AllCourses({ }: Props) {
                     Explore Categories:
                 </div>
                 <CarouselSmItem>
-                    <DynamicCard className="bg-white group text-black rounded-lg transition cursor-pointer p-4 m-2 hover:bg-main-primary-yellow">
-                        <div className="flex text-left gap-3">
-                            <div className="p-2 rounded-lg bg-main-primary-yellow group-hover:bg-white">
-                                <FaGlobe className="text-2xl" />
-                            </div>
-                            <div className="flex-1">
-                                <div className="font-semibold">IT</div>
-                                <div className="text-xs">1,250 Courses</div>
-                            </div>
-                        </div>
-                    </DynamicCard>
-                    <DynamicCard className="bg-white group text-black rounded-lg transition cursor-pointer p-4 m-2 hover:bg-main-primary-yellow">
-                        <div className="flex text-left gap-3">
-                            <div className="p-2 rounded-lg bg-main-primary-yellow group-hover:bg-white">
-                                <FaGlobe className="text-2xl" />
-                            </div>
-                            <div className="flex-1">
-                                <div className="font-semibold">IT</div>
-                                <div className="text-xs">1,250 Courses</div>
-                            </div>
-                        </div>
-                    </DynamicCard>
-                    <DynamicCard className="bg-white group text-black rounded-lg transition cursor-pointer p-4 m-2 hover:bg-main-primary-yellow">
-                        <div className="flex text-left gap-3">
-                            <div className="p-2 rounded-lg bg-main-primary-yellow group-hover:bg-white">
-                                <FaGlobe className="text-2xl" />
-                            </div>
-                            <div className="flex-1">
-                                <div className="font-semibold">IT</div>
-                                <div className="text-xs">1,250 Courses</div>
-                            </div>
-                        </div>
-                    </DynamicCard>
-                    <DynamicCard className="bg-white group text-black rounded-lg transition cursor-pointer p-4 m-2 hover:bg-main-primary-yellow">
-                        <div className="flex text-left gap-3">
-                            <div className="p-2 rounded-lg bg-main-primary-yellow group-hover:bg-white">
-                                <FaGlobe className="text-2xl" />
-                            </div>
-                            <div className="flex-1">
-                                <div className="font-semibold">IT</div>
-                                <div className="text-xs">1,250 Courses</div>
-                            </div>
-                        </div>
-                    </DynamicCard>
-                    <DynamicCard className="bg-white group text-black rounded-lg transition cursor-pointer p-4 m-2 hover:bg-main-primary-yellow">
-                        <div className="flex text-left gap-3">
-                            <div className="p-2 rounded-lg bg-main-primary-yellow group-hover:bg-white">
-                                <FaGlobe className="text-2xl" />
-                            </div>
-                            <div className="flex-1">
-                                <div className="font-semibold">IT</div>
-                                <div className="text-xs">1,250 Courses</div>
-                            </div>
-                        </div>
-                    </DynamicCard>
-                    <DynamicCard className="bg-white group text-black rounded-lg transition cursor-pointer p-4 m-2 hover:bg-main-primary-yellow">
-                        <div className="flex text-left gap-3">
-                            <div className="p-2 rounded-lg bg-main-primary-yellow group-hover:bg-white">
-                                <FaGlobe className="text-2xl" />
-                            </div>
-                            <div className="flex-1">
-                                <div className="font-semibold">IT</div>
-                                <div className="text-xs">1,250 Courses</div>
-                            </div>
-                        </div>
-                    </DynamicCard>
-                    <DynamicCard className="bg-white group text-black rounded-lg transition cursor-pointer p-4 m-2 hover:bg-main-primary-yellow">
-                        <div className="flex text-left gap-3">
-                            <div className="p-2 rounded-lg bg-main-primary-yellow group-hover:bg-white">
-                                <FaGlobe className="text-2xl" />
-                            </div>
-                            <div className="flex-1">
-                                <div className="font-semibold">IT</div>
-                                <div className="text-xs">1,250 Courses</div>
-                            </div>
-                        </div>
-                    </DynamicCard>
-                    <DynamicCard className="bg-white group text-black rounded-lg transition cursor-pointer p-4 m-2 hover:bg-main-primary-yellow">
-                        <div className="flex text-left gap-3">
-                            <div className="p-2 rounded-lg bg-main-primary-yellow group-hover:bg-white">
-                                <FaGlobe className="text-2xl" />
-                            </div>
-                            <div className="flex-1">
-                                <div className="font-semibold">IT</div>
-                                <div className="text-xs">1,250 Courses</div>
-                            </div>
-                        </div>
-                    </DynamicCard>
-                    <DynamicCard className="bg-white group text-black rounded-lg transition cursor-pointer p-4 m-2 hover:bg-main-primary-yellow">
-                        <div className="flex text-left gap-3">
-                            <div className="p-2 rounded-lg bg-main-primary-yellow group-hover:bg-white">
-                                <FaGlobe className="text-2xl" />
-                            </div>
-                            <div className="flex-1">
-                                <div className="font-semibold">IT</div>
-                                <div className="text-xs">1,250 Courses</div>
-                            </div>
-                        </div>
-                    </DynamicCard>
-
+                    <SmCard />
+                    <SmCard />
+                    <SmCard />
+                    <SmCard />
+                    <SmCard />
                 </CarouselSmItem>
             </PageHeader>
         </section>
