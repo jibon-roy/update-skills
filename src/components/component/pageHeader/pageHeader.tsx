95f436ac12498c3292243d0637ab3e2f136efadd
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-06-07 16:38:25 +0600

    page heading updating

--- src/components/component/pageHeader/pageHeader.tsx
+++ src/components/component/pageHeader/pageHeader.tsx
@@ -1,5 +1,6 @@
-
+"use client"
 import FlexSimple from '@/components/sections/flex-simple'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 
 type Props = {
@@ -8,12 +9,16 @@ type Props = {
 }
 
 const PageHeader: React.FC<Props> = ({ heading, children }: Props) => {
+
+    const location = usePathname()
+    console.log(location)
     return (
         <div className='bg-main-primary-violet'>
             <FlexSimple flexDirection='column' flexDirectionMd='column' className='text-center py-10 text-white' justifyContent='center' alignItems='center'>
                 <h1 className='mb-10 font-semibold text-4xl'>
                     {heading}
                 </h1>
+                { }
                 {children}
             </FlexSimple>
         </div>
